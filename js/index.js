eaa11ff83973e141585fc5deb9e78819eee8dda1
Kareem Grant <kareem@getuserwise.com>
2019-08-20 18:28:57 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('#grayButton').click(() => {
+    $('body').attr('class', 'gray')
+  })
+
+  // 1) Add the code to accomodate using jQuery
+  //    to make the other buttons work as expected
+
+  // 2) Add two additional buttons
+
+  // Extra Credit
+  // - Add a link labeled "Show / Hide Buttons"
+  // - When users click on the link the following should happen
+  // -- if the buttons are showing, then pressing the link should Hide them
+  // -- if the buttons are not showing, then pressing the link should Show them
+  // -- Hint: Google is your friend
+})
